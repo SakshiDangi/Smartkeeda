731c05675f284295408cdbc0c7cfc7a0d97404d3
sakshi <panchalsakshi01@gmail.com>
2023-08-21 22:36:12 +0530

    home added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+
 
 function App() {
   return (
     <div className='App'>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/marketing' element={<Marketing />} />
+        <Route path='/consulting' element={<Consulting />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className='home'>
+        <div className='left'>
+        <h1>Unveil <span>Your</span> <span>Potential</span>, Rewrite <span>Possibilities</span></h1>
+        <p>Highlighting our role in helping individuals tap into their hidden potential, thus expanding the realm of what they can achieve.</p>
+        <button>Get Started</button>
+        <button>Take Test Series</button>
+        <div className='fa'></div>
+        </div>
+      </div>
+    </>
+  );
+}
