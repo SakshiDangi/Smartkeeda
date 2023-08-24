ca566c91c3313f9622a79dc65eb3cf8e2808b214
sakshi <panchalsakshi01@gmail.com>
2023-08-24 23:41:30 +0530

    first

---
+++ src/components/pages/counselling.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const counselling = () => {
+  return (
+    <div>counselling</div>
+  )
+}
+
+export default counselling
---
+++ src/components/pages/testSeries.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testSeries = () => {
+  return (
+    <div>testSeries</div>
+  )
+}
+
+export default testSeries
